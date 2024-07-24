ba87b487a21cf5c1d8f8ebe338789c2ccaa9353a
Andreea-Ioana Gheorghe <163008395+andreea-ghe@users.noreply.github.com>
2024-07-24 22:59:57 +0300

    Update post-comments.js

--- post-comments.js
+++ post-comments.js
@@ -1,6 +1,5 @@
-import { Octokit } from '@octokit/core';
-
 async function postCommentToGitHub(escaped_comments, commit_id, file_path, start_line, side) {
+  const { Octokit } = await import('@octokit/core');
   const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
   
   console.log(escaped_comments);
