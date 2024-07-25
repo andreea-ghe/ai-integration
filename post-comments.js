a173d9f7d2a72ac444bea1fdfa37980920c3aa40
Andreea-Ioana Gheorghe <163008395+andreea-ghe@users.noreply.github.com>
2024-07-25 11:50:51 +0300

    Update post-comments.js

--- post-comments.js
+++ post-comments.js
@@ -9,16 +9,26 @@ async function postCommentToGitHub(escaped_comments, commit_id, file_path, start
   console.log(side);
   
   const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
-
+  pull_number = process.env.GITHUB_EVENT_NUMBER;
+  
   console.log(owner);
   console.log(repo);
   
-  const response = await octokit.issues.createComment({
-        repo,
-        owner,
-        issue_number: process.env.GITHUB_EVENT_NUMBER,
-        body: escaped_comments
-        });
+  const response = await octokit.request('POST /repos/{owner}/{repo}/pulls/{pull_number}/comments', {
+    owner,
+    repo,
+    pull_number,
+    body: 'Great stuff!',
+    commit_id: process.env.COMMIT_ID,
+    path: 'goodbye.txt',
+    start_line: 1,
+    start_side: 'RIGHT',
+    line: 2,
+    side: 'RIGHT',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
 
   if (response.status !== 201) {
     throw new Error(`GitHub API responded with ${response.status}: ${response.statusText}`);
