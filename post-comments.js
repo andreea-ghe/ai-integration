fbf99450b8a203b1cb73788f4247d2867b0df1c0
Andreea-Ioana Gheorghe <163008395+andreea-ghe@users.noreply.github.com>
2024-07-25 21:24:06 +0300

    Update post-comments.js

--- post-comments.js
+++ post-comments.js
@@ -11,8 +11,8 @@ async function postCommentToGitHub(comment_body, commit_id, file_path, start_lin
     const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
     const pull_number = process.env.GITHUB_EVENT_NUMBER;
 
-    # console.log('Repository Info:', { owner, repo, pull_number });
-    # console.log(String(comment_body));
+    // console.log('Repository Info:', { owner, repo, pull_number });
+    // console.log(String(comment_body));
 
     let response;
     if (start_line == line) {
