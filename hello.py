122bbdb5bb2f76b59d6df99c4f2a692145d59ebc
Andreea-Ioana Gheorghe <163008395+andreea-ghe@users.noreply.github.com>
2024-07-25 14:49:52 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,56 +1,155 @@
-def merge(array, left, mid, right):
-    subArrayOne = mid - left + 1
-    subArrayTwo = right - mid
+import { Context, Probot } from 'probot';
 
-    # Create temp arrays
-    leftArray = [0] * subArrayOne
-    rightArray = [0] * subArrayTwo
+import { Chat } from './chat.js';
 
-    # Copy data to temp arrays leftArray[] and rightArray[]
-    for i in range(subArrayOne):
-        leftArray[i] = array[left + i]
-    for j in range(subArrayTwo):
-        rightArray[j] = array[mid + 1 + j]
+const OPENAI_API_KEY = 'OPENAI_API_KEY';
+const MAX_PATCH_COUNT = process.env.MAX_PATCH_LENGTH
+  ? +process.env.MAX_PATCH_LENGTH
+  : Infinity;
 
-    indexOfSubArrayOne = 0  
-    indexOfSubArrayTwo = 0  
-    indexOfMergedArray = left 
+export const robot = (app: Probot) => {
+  const loadChat = async (context: Context) => {
+    if (process.env.OPENAI_API_KEY) {
+      return new Chat(process.env.OPENAI_API_KEY);
+    }
 
-    # Merge the temp arrays back into array[left..right]
-    while indexOfSubArrayOne < subArrayOne and indexOfSubArrayTwo < subArrayTwo:
-        if leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo]:
-            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne]
-            indexOfSubArrayOne += 1
-        else:
-            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo]
-            indexOfSubArrayTwo += 1
-        indexOfMergedArray += 1
+    const repo = context.repo();
 
-    # Copy the remaining elements of left[], if any
-    while indexOfSubArrayOne < subArrayOne:
-        array[indexOfMergedArray] = leftArray[indexOfSubArrayOne]
-        indexOfSubArrayOne += 1
-        indexOfMergedArray += 1
+    try {
+      const { data } = (await context.octokit.request(
+        'GET /repos/{owner}/{repo}/actions/variables/{name}',
+        {
+          owner: repo.owner,
+          repo: repo.repo,
+          name: OPENAI_API_KEY,
+        }
+      )) as any;
 
-    # Copy the remaining elements of right[], if any
-    while indexOfSubArrayTwo < subArrayTwo:
-        array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo]
-        indexOfSubArrayTwo += 1
-        indexOfMergedArray += 1
+      if (!data?.value) {
+        return null;
+      }
 
-# begin is for left index and end is right index
-# of the sub-array of arr to be sorted
-def mergeSort(array, begin, end):
-    if begin >= end:
-        return
+      return new Chat(data.value);
+    } catch {
+      await context.octokit.issues.createComment({
+        repo: repo.repo,
+        owner: repo.owner,
+        issue_number: context.pullRequest().pull_number,
+        body: `Seems you are using me but didn't get OPENAI_API_KEY seted in Variables/Secrets for this repo. you could follow [readme](https://github.com/anc95/ChatGPT-CodeReview) for more information`,
+      });
+      return null;
+    }
+  };
 
-    mid = begin + (end - begin) // 2
-    mergeSort(array, begin, mid)
-    mergeSort(array, mid + 1, end)
-    merge(array, begin, mid, end)
+  app.on(
+    ['pull_request.opened', 'pull_request.synchronize'],
+    async (context) => {
+      const repo = context.repo();
+      const chat = await loadChat(context);
 
-# Function to print an array
-def printArray(array, size):
-    for i in range(size):
-        print(array[i], end=" ")
-    print()
+      if (!chat) {
+        console.log('Chat initialized failed');
+        return 'no chat';
+      }
+
+      const pull_request = context.payload.pull_request;
+
+      if (
+        pull_request.state === 'closed' ||
+        pull_request.locked
+      ) {
+        console.log('invalid event payload');
+        return 'invalid event payload';
+      }
+
+      const target_label = process.env.TARGET_LABEL;
+      if (
+        target_label &&
+        (!pull_request.labels?.length ||
+          pull_request.labels.every((label) => label.name !== target_label))
+      ) {
+        console.log('no target label attached');
+        return 'no target label attached';
+      }
+
+      const data = await context.octokit.repos.compareCommits({
+        owner: repo.owner,
+        repo: repo.repo,
+        base: context.payload.pull_request.base.sha,
+        head: context.payload.pull_request.head.sha,
+      });
+
+      let { files: changedFiles, commits } = data.data;
+
+      if (context.payload.action === 'synchronize' && commits.length >= 2) {
+        const {
+          data: { files },
+        } = await context.octokit.repos.compareCommits({
+          owner: repo.owner,
+          repo: repo.repo,
+          base: commits[commits.length - 2].sha,
+          head: commits[commits.length - 1].sha,
+        });
+
+        const ignoreList = (process.env.IGNORE || process.env.ignore || '')
+          .split('\n')
+          .filter((v) => v !== '');
+
+        const filesNames = files?.map((file) => file.filename) || [];
+        changedFiles = changedFiles?.filter(
+          (file) =>
+            filesNames.includes(file.filename) &&
+            !ignoreList.includes(file.filename)
+        );
+      }
+
+      if (!changedFiles?.length) {
+        console.log('no change found');
+        return 'no change';
+      }
+
+      console.time('gpt cost');
+
+      for (let i = 0; i < changedFiles.length; i++) {
+        const file = changedFiles[i];
+        const patch = file.patch || '';
+
+        if (file.status !== 'modified' && file.status !== 'added') {
+          continue;
+        }
+
+        if (!patch || patch.length > MAX_PATCH_COUNT) {
+          console.log(
+            `${file.filename} skipped caused by its diff is too large`
+          );
+          continue;
+        }
+        try {
+          const res = await chat?.codeReview(patch);
+
+          if (!!res) {
+            await context.octokit.pulls.createReviewComment({
+              repo: repo.repo,
+              owner: repo.owner,
+              pull_number: context.pullRequest().pull_number,
+              commit_id: commits[commits.length - 1].sha,
+              path: file.filename,
+              body: res,
+              position: patch.split('\n').length - 1,
+            });
+          }
+        } catch (e) {
+          console.error(`review ${file.filename} failed`, e);
+        }
+      }
+
+      console.timeEnd('gpt cost');
+      console.info(
+        'successfully reviewed',
+        context.payload.pull_request.html_url
+      );
+
+      return 'success';
+    }
+  );
+};
